d374439ef330c10f4c91f09b5d54ac69b317bfdf
shangfeiSF <tony20100125@126.com>
2016-08-21 23:08:08 +0800

    leran react-router step by step

--- lessons/4.nested-routes/index.js
+++ lessons/4.nested-routes/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { Router, Route, hashHistory } from 'react-router'
+import {render} from 'react-dom'
+
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
 
-render((
+import {Router, Route, hashHistory} from 'react-router'
+var content = (
   <Router history={hashHistory}>
-    <Route path="/" component={App}/>
-    <Route path="/repos" component={Repos}/>
-    <Route path="/about" component={About}/>
+    <Route path="/" component={App}>
+      <Route path="/repos" component={Repos}/>
+      <Route path="/about" component={About}/>
+    </Route>
   </Router>
-), document.getElementById('app'))
+)
+var root = document.getElementById('app')
+
+render(content, root)
+
