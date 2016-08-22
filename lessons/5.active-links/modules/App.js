cba81a02b3a92b45e20ad2e73836584285d4a016
shangfeiSF <tony20100125@126.com>
2016-08-22 12:35:48 +0800

    leran react-router step by step

--- lessons/5.active-links/modules/App.js
+++ lessons/5.active-links/modules/App.js
@@ -1,14 +1,21 @@
 import React from 'react'
-import { Link } from 'react-router'
+import {Link} from 'react-router'
+
+import NavLink from './NavLink'
 
 export default React.createClass({
   render() {
+    var activeStyle = {color: 'red'}
+    var activeClassName = 'current'
+
     return (
       <div>
-        <h1>React Router Tutorial</h1>
+        <h1>React Router Lessons</h1>
         <ul role="nav">
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/repos">Repos</Link></li>
+          <li><Link to="/about" activeClassName={activeClassName} activeStyle={activeStyle}>About</Link></li>
+          <li><Link to="/repos" activeClassName={activeClassName} activeStyle={activeStyle}>Repos</Link></li>
+          <li><NavLink to="/about" activeClassName={activeClassName}>About</NavLink></li>
+          <li><NavLink to="/repos" activeClassName={activeClassName}>Repos</NavLink></li>
         </ul>
         {this.props.children}
       </div>
