820f2dac2f1c533165e9dbf3615929342016e469
shangfeiSF <tony20100125@126.com>
2016-08-22 14:42:12 +0800

    leran react-router step by step

--- lessons/10.clean-urls/index.js
+++ lessons/10.clean-urls/index.js
@@ -1,14 +1,15 @@
 import React from 'react'
-import { render } from 'react-dom'
-import { Router, Route, hashHistory, IndexRoute } from 'react-router'
+import {render} from 'react-dom'
+
 import App from './modules/App'
 import About from './modules/About'
 import Repos from './modules/Repos'
 import Repo from './modules/Repo'
 import Home from './modules/Home'
 
-render((
-  <Router history={hashHistory}>
+import {Router, Route, browserHistory, IndexRoute} from 'react-router'
+var content = (
+  <Router history={browserHistory}>
     <Route path="/" component={App}>
       <IndexRoute component={Home}/>
       <Route path="/repos" component={Repos}>
@@ -17,4 +18,8 @@ render((
       <Route path="/about" component={About}/>
     </Route>
   </Router>
-), document.getElementById('app'))
+)
+var root = document.getElementById('app')
+
+render(content, root)
+
