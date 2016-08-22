fda8a63cd34741a988565fb925752ad1b5f6b938
shangfeiSF <tony20100125@126.com>
2016-08-22 16:12:29 +0800

    begin to review and format

--- lessons/12.navigating/modules/Repos.js
+++ lessons/12.navigating/modules/Repos.js
@@ -1,14 +1,32 @@
 import React from 'react'
-import NavLink from './NavLink'
+import {Link, browserHistory} from 'react-router'
 
 export default React.createClass({
+
+  handleSubmit(event) {
+    event.preventDefault()
+    const userName = event.target.elements[0].value
+    const repo = event.target.elements[1].value
+    const path = `/repos/${userName}/${repo}`
+    console.log(path)
+    browserHistory.push(path)
+  },
+
   render() {
     return (
       <div>
         <h2>Repos</h2>
-        <ul>
-          <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
-          <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+        <ul role="nav">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/repos/facebook/react">React</Link></li>
+          <li><Link to="/repos/reactjs/react-router">React Router</Link></li>
+          <li>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" placeholder="userName"/>
+              <input type="text" placeholder="repo"/>
+              <button type="submit">Go</button>
+            </form>
+          </li>
         </ul>
         {this.props.children}
       </div>
