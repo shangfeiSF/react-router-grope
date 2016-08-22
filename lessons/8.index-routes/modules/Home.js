c9ac4ae4f0e761b333560a2442eaa8a8fd838b7f
shangfeiSF <tony20100125@126.com>
2016-08-22 14:25:58 +0800

    leran react-router step by step

--- lessons/8.index-routes/modules/Home.js
+++ lessons/8.index-routes/modules/Home.js
@@ -1,8 +1,7 @@
 import React from 'react'
-import {Link} from 'react-router'
 
 export default React.createClass({
   render() {
-    return <Link {...this.props}/>
+    return <div>Home</div>
   }
 })
